2fa695c4a7f0fd9b51056a9711c23482243f652e
lep <lep2014@163.com>
2016-03-23 17:02:23 +0800

    hibernate maping

--- src/org/lep/hibernate/model/Address.java
+++ src/org/lep/hibernate/model/Address.java
@@ -1,10 +1,14 @@
 package org.lep.hibernate.model;
 
+import java.util.Set;
+
 /**
  * Created by lvep on 2016/3/21.
  */
 public class Address {
     private long id;
+    private Set<Person> people;
+    private Person person;
 
     public long getId() {
         return id;
@@ -12,5 +16,21 @@ public class Address {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public Set<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(Set<Person> people) {
+        this.people = people;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
     }
 }
